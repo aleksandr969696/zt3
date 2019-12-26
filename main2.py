66eeb54589cc3cca9b8b0cb9412c12a8ba2290bb
aleks <efimovaleksandr@room42.ru>
2019-12-26 15:43:00 +0300

    6

--- main2.py
+++ main2.py
@@ -80,98 +80,97 @@ def plot_solutions(u_e, u, mesh):
 
 
 # Problem implementing
-def main(r):
-    alpha_val = 1
-    T = 1.0
-    dt = T / 100
-    u_e = sympy.sin(x) ** 2 + sympy.cos(y) ** 2
+# def main(r):
+alpha_val = 1
+T = 1.0
+dt = T / 100
+u_e = sympy.sin(x) ** 2 + sympy.cos(y) ** 2
 
-    circle = Circle(Point(0, 0), r)
-    mesh = generate_mesh(circle, 30)
-    V = FunctionSpace(mesh, 'P', 2)
-    u_D = Expression(ccode(u_e), degree=2)
-    f = -get_lap(u_e) + alpha * u_e
+circle = Circle(Point(0, 0), r)
+mesh = generate_mesh(circle, 30)
+V = FunctionSpace(mesh, 'P', 2)
+u_D = Expression(ccode(u_e), degree=2)
+f = -get_lap(u_e) + alpha * u_e
 
-    bc = DirichletBC(V, u_D, boundary)
-    u = TrialFunction(V)
-    v = TestFunction(V)
-    f = Expression(ccode(f), degree=2, alpha=alpha_val)
-    gradient = get_grad(u_e)
-    g = Expression(
-        f'{ccode(gradient[0])} * x[0] / sqrt(x[0]*x[0] + x[1]*x[1]) + {ccode(gradient[1])} * x[1] / sqrt(x[0]*x[0] + x[1]*x[1])',
-        degree=2)
-    a = dot(grad(u), grad(v)) * dx + alpha_val * u * v * dx
-    L = f * v * dx + g * v * ds
-    u = Function(V)
+bc = DirichletBC(V, u_D, boundary)
+u = TrialFunction(V)
+v = TestFunction(V)
+f = Expression(ccode(f), degree=2, alpha=alpha_val)
+gradient = get_grad(u_e)
+g = Expression(
+    f'{ccode(gradient[0])} * x[0] / sqrt(x[0]*x[0] + x[1]*x[1]) + {ccode(gradient[1])} * x[1] / sqrt(x[0]*x[0] + x[1]*x[1])',
+    degree=2)
+a = dot(grad(u), grad(v)) * dx + alpha_val * u * v * dx
+L = f * v * dx + g * v * ds
+u = Function(V)
+solve(a == L, u, bc)
+
+# Accuracy calculation
+
+error_L2 = errornorm(u_D, u, 'L2')
+vertex_values_u_D = u_D.compute_vertex_values(mesh)
+vertex_values_u = u.compute_vertex_values(mesh)
+error_C = np.max(np.abs(vertex_values_u - vertex_values_u_D))
+
+print(f'L2 error = {error_L2}')
+print(f'C error = {error_C}')
+
+# Visualisation
+
+plot(u, is_accurate=False, to_save=True)
+plot(u_D, is_accurate=True, to_save=True)
+
+# Thermal conductivity
+steps_number = 50
+# u_e = sympy.sin(x)**2 + sympy.cos(y)**2 + t
+u_e = x * sympy.cos(t ** 2) + t * sympy.sin(y)
+
+u_D = Expression(ccode(u_e), degree=2, t=0)
+bc = DirichletBC(V, u_D, boundary)
+u_n = interpolate(u_D, V)
+u = TrialFunction(V)
+v = TestFunction(V)
+
+f = Expression(ccode(sympy.diff(u_e, t) - alpha * get_lap(u_e)), alpha=alpha_val, degree=2, t=0)
+gradient = get_gradient(u_e)
+g = Expression(
+    f'{ccode(gradient[0])} * x[0] / sqrt(x[0]*x[0] + x[1]*x[1]) + {ccode(gradient[1])} * x[1] / sqrt(x[0]*x[0] + x[1]*x[1])',
+    degree=2, t=0
+)
+
+T = 5.0
+dt = T / steps_number
+a = u * v * dx + dt * dot(grad(u), grad(v)) * dx
+L = (u_n + dt * f) * v * dx + dt * v * g * ds
+
+sympy.diff(u_e, t)
+
+F = u * v * dx + dt * dot(grad(u), grad(v)) * dx - (u_n + dt * f) * v * dx - dt * v * g * ds
+a, L = lhs(F), rhs(F)
+u = Function(V)
+t = 0
+plots = []
+errors_L2 = []
+errors_max = []
+
+for n in range(steps_number):
+    t += dt
+    u_D.t = t
+    g.t = t
+    f.t = t
     solve(a == L, u, bc)
+    u_n.assign(u)
+    u_e = interpolate(u_D, V)
+    plots.append(plot_solutions(u_e, u, mesh))
+    errors_L2.append(errornorm(u_e, u, 'L2'))
+    errors_max.append(np.abs(u_e.vector().get_local() - u.vector().get_local()).max())
 
-    # Accuracy calculation
+get_animation(plots, 'my_plots')
 
-    error_L2 = errornorm(u_D, u, 'L2')
-    vertex_values_u_D = u_D.compute_vertex_values(mesh)
-    vertex_values_u = u.compute_vertex_values(mesh)
-    error_C = np.max(np.abs(vertex_values_u - vertex_values_u_D))
-
-    print(f'L2 error = {error_L2}')
-    print(f'C error = {error_C}')
-
-    # Visualisation
-
-    plot(u, is_accurate=False, to_save=True)
-    plot(u_D, is_accurate=True, to_save=True)
-
-    # Thermal conductivity
-    steps_number = 50
-    # u_e = sympy.sin(x)**2 + sympy.cos(y)**2 + t
-    u_e = x * sympy.cos(t ** 2) + t * sympy.sin(y)
-
-    u_D = Expression(ccode(u_e), degree=2, t=0)
-    bc = DirichletBC(V, u_D, boundary)
-    u_n = interpolate(u_D, V)
-    u = TrialFunction(V)
-    v = TestFunction(V)
-
-    f = Expression(ccode(sympy.diff(u_e, t) - alpha * get_lap(u_e)), alpha=alpha_val, degree=2, t=0)
-    gradient = get_gradient(u_e)
-    g = Expression(
-        f'{ccode(gradient[0])} * x[0] / sqrt(x[0]*x[0] + x[1]*x[1]) + {ccode(gradient[1])} * x[1] / sqrt(x[0]*x[0] + x[1]*x[1])',
-        degree=2, t=0
-    )
-
-    T = 5.0
-    dt = T / steps_number
-    a = u * v * dx + dt * dot(grad(u), grad(v)) * dx
-    L = (u_n + dt * f) * v * dx + dt * v * g * ds
-
-    sympy.diff(u_e, t)
-
-    F = u * v * dx + dt * dot(grad(u), grad(v)) * dx - (u_n + dt * f) * v * dx - dt * v * g * ds
-    a, L = lhs(F), rhs(F)
-    u = Function(V)
-    t = 0
-    plots = []
-    errors_L2 = []
-    errors_max = []
-
-    for n in range(steps_number):
-        t += dt
-        u_D.t = t
-        g.t = t
-        f.t = t
-        solve(a == L, u, bc)
-        u_n.assign(u)
-        u_e = interpolate(u_D, V)
-        plots.append(plot_solutions(u_e, u, mesh))
-        errors_L2.append(errornorm(u_e, u, 'L2'))
-        errors_max.append(np.abs(u_e.vector().get_local() - u.vector().get_local()).max())
-
-    get_animation(plots, 'my_plots')
-
-    time_arr = np.linspace(dt, T, steps_number)
-    plt.plot(time_arr, errors_max, label='Max errors')
-    plt.plot(time_arr, errors_L2, label='L2 errors')
-    plt.legend()
-    plt.savefig(f'errors.png')
+time_arr = np.linspace(dt, T, steps_number)
+plt.plot(time_arr, errors_max, label='Max errors')
+plt.plot(time_arr, errors_L2, label='L2 errors')
+plt.legend()
+plt.savefig(f'errors.png')
 
 
-main(1)
